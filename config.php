ae7098cc5bb4823d24af0afe6ed5e4b9b515287e
Amir666sa <95975822+Amir666sa@users.noreply.github.com>
2021-12-14 17:00:06 +0330

    Add files via upload

--- config.php
+++ config.php
@@ -1,14 +1,20 @@
 <?php
-
-$config = [
-    "auth" => [
-        METHOD_NO_AUTH => true,
-        METHOD_USER_PASS => function ($request) {
-            return $request['user'] == 'user' && $request['pass'] == 'pass';
-        }
-    ],
-    "log_level" => LOG_DEBUG,
-    "tcp_port" => 1080,
-    "udp_port" => 0,
-    "wanIP" => '192.168.1.1',
-];
+error_reporting(0);
+//========================== // token // ==============================
+define('5024774686:AAFymudTpVwFUYdZM1H3hdBEW6_VuyWPvU4');//توکن بزارید
+//========================== // config // ==============================
+$admin = ['911478720','2100323908']; // ها را ماننده این الگورتیم بگذارید ادمین اصلی ایدی اول است
+$usernamebot = '@Smsbom_saCheese_bot'; // یوزرنیم ربات
+$channel = 'testbotmyahmad'; // ایدی کانال بدون @
+$channelname = 'botbom'; // تگزو سورس
+$botname = 'Smsbom'; //تگزو سوری
+$adpush = 'fake_XQ_QM'; //ایدی پشتیبانی بدون@
+$web = 'https://domin.com/پوشه/bot.php'; // آدرس محل قرار گیری سورس همراه با پوشه
+$logozir = 'https://telegra.ph/file/a10e4ab4e721641306f04.jpg';//لینک عکس زیر مجموعه گیری
+$db_user = 'sayazdan_amir667'; //نام یوزر
+$pass = 'AHMAD2005'; //رمز دیتابیس
+$db_name = 'sayazdan_amir667'; //نام دیتا
+//========================== // database // ==============================
+$connect = new mysqli('localhost',$db_name,$pass,$db_user);
+$connect->query("SET NAMES 'utf8'"); $connect->set_charset('utf8mb4');
+?>i
